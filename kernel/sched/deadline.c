c81f6a79475aeeb0243c085554da5eb1641bd1dc
Wanpeng Li <wanpeng.li@hotmail.com>
2016-04-22 17:07:24 +0800

    sched/cpufreq: Optimize cpufreq update kicker to avoid update multiple times
    Sometimes delta_exec is 0 due to update_curr() is called multiple times,
    this is captured by:
    
    	u64 delta_exec = rq_clock_task(rq) - curr->se.exec_start;
    
    This patch optimizes the cpufreq update kicker by bailing out when nothing
    changed, it will benefit the upcoming schedutil, since otherwise it will
    (over)react to the special util/max combination.
    
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1461316044-9520-1-git-send-email-wanpeng.li@hotmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -860,10 +860,6 @@ static void update_curr_dl(struct rq *rq)
 	if (!dl_task(curr) || !on_dl_rq(dl_se))
 		return;
 
-	/* Kick cpufreq (see the comment in linux/cpufreq.h). */
-	if (cpu_of(rq) == smp_processor_id())
-		cpufreq_trigger_update(rq_clock(rq));
-
 	/*
 	 * Consumed budget is computed considering the time as
 	 * observed by schedulable tasks (excluding time spent
@@ -878,6 +874,10 @@ static void update_curr_dl(struct rq *rq)
 
 	/* kick cpufreq (see the comment in kernel/sched/sched.h). */
 	cpufreq_update_this_cpu(rq, SCHED_CPUFREQ_DL);
+
+	/* kick cpufreq (see the comment in linux/cpufreq.h). */
+	if (cpu_of(rq) == smp_processor_id())
+		cpufreq_trigger_update(rq_clock(rq));
 
 	schedstat_set(curr->se.statistics.exec_max,
 		      max(curr->se.statistics.exec_max, delta_exec));
