030def85a38eb1ab2be00c528045d167a3a40974
celtare21 <celtare21@gmail.com>
2018-05-03 19:38:34 +0000

    Revert "nospec: Allow index argument to have const-qualified type"
    This reverts commit 8b3e004a49ed08352db0ec672449e198a111d8da.

--- include/linux/nospec.h
+++ include/linux/nospec.h
@@ -66,6 +66,7 @@ static inline unsigned long array_index_mask_nospec(unsigned long index,
 	BUILD_BUG_ON(sizeof(_i) > sizeof(long));			\
 	BUILD_BUG_ON(sizeof(_s) > sizeof(long));			\
 									\
-	(typeof(_i)) (_i & _mask);					\
+	_i &= _mask;							\
+	_i;								\
 })
 #endif /* _LINUX_NOSPEC_H */
