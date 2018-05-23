70e6c0796d559b63880f941c5fc9903c094a869c
celtare21 <celtare21@gmail.com>
2018-05-23 17:50:32 +0000

    Revert "xen-netfront: Fix hang on device removal"
    This reverts commit 494e09d1617f4bc5151f19b61d1aef56aa24fae7.

--- drivers/net/xen-netfront.c
+++ drivers/net/xen-netfront.c
@@ -2024,10 +2024,7 @@ static void netback_changed(struct xenbus_device *dev,
 	case XenbusStateInitialised:
 	case XenbusStateReconfiguring:
 	case XenbusStateReconfigured:
-		break;
-
 	case XenbusStateUnknown:
-		wake_up_all(&module_unload_q);
 		break;
 
 	case XenbusStateInitWait:
@@ -2158,9 +2155,7 @@ static int xennet_remove(struct xenbus_device *dev)
 		xenbus_switch_state(dev, XenbusStateClosing);
 		wait_event(module_unload_q,
 			   xenbus_read_driver_state(dev->otherend) ==
-			   XenbusStateClosing ||
-			   xenbus_read_driver_state(dev->otherend) ==
-			   XenbusStateUnknown);
+			   XenbusStateClosing);
 
 		xenbus_switch_state(dev, XenbusStateClosed);
 		wait_event(module_unload_q,
