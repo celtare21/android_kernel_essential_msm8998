fb6c132970b8d8b2cf95df127974a9b9849d87c0
celtare21 <celtare21@gmail.com>
2018-06-18 22:12:48 +0000

    Import wifi stack from stock

--- drivers/staging/qcacld-3.0/core/utils/fwlog/dbglog_host.c
+++ drivers/staging/qcacld-3.0/core/utils/fwlog/dbglog_host.c
@@ -61,12 +61,11 @@
 #include <a_debug.h>
 #define FWLOG_DEBUG   ATH_DEBUG_MAKE_MODULE_MASK(0)
 
+#ifdef WLAN_DEBUG
+
 static int get_version;
 static int gprint_limiter;
 static bool tgt_assert_enable;
-
-#ifdef WLAN_DEBUG
-
 static ATH_DEBUG_MASK_DESCRIPTION g_fwlog_debug_description[] = {
 	{FWLOG_DEBUG, "fwlog"},
 };
@@ -4213,7 +4212,7 @@ static void cnss_diag_cmd_handler(const void *data, int data_len,
 	 * audit note: it is ok to pass a NULL policy here since a
 	 * length check on the data is added later already
 	 */
-	if (nla_parse(tb, CLD80211_ATTR_MAX, data, data_len, NULL)) {
+	if (hdd_nla_parse(tb, CLD80211_ATTR_MAX, data, data_len, NULL)) {
 		AR_DEBUG_PRINTF(ATH_DEBUG_ERR, ("%s: nla parse fails\n",
 							__func__));
 		return;
