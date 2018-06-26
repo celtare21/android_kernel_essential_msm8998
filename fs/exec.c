33498550ee543d2332bb432c5c463fd3b48edf29
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-27 00:28:42 +0900

    kernel: Boost all CPUs to the max when userspace launches an app[DIFF]
    Zygote is Android's application process, and it only forks when launching
    a new app for the first time. To accelerate app launches, boost all CPUs
    to the max for 1250 ms when zygote decides to fork.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1647,10 +1647,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (retval < 0)
 		goto out;
 
-	if (!strcmp(filename->name, ZYGOTE32_BIN))
-		atomic_set(&zygote32_pid, current->pid);
-	else if (!strcmp(filename->name, ZYGOTE64_BIN))
-		atomic_set(&zygote64_pid, current->pid);
+	if (capable(CAP_SYS_ADMIN)) {
+		if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN)))
+			atomic_set(&zygote32_pid, current->pid);
+		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
+			atomic_set(&zygote64_pid, current->pid);
+	}
+
 
 	/* execve succeeded */
 	current->fs->in_exec = 0;
